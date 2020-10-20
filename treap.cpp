25340bb23d1e1b7546fa32167633d168e6c22d02
grumpy_gordon <ramil-pauchok@mail.ru>
2020-10-20 12:34:50 +0300

    treap update

--- treap.cpp
+++ treap.cpp
@@ -1,59 +1,92 @@
-struct treap{
-    int x, y, mn;
+struct treap {
+    int y, sz;
+    ll x, s;
     treap *l, *r;
 };
 
-typedef treap * ptr;
+typedef treap *ptr;
 
-ptr create(int x){
+ptr create(ll x) {
     ptr t = new treap;
     t->l = nullptr;
     t->r = nullptr;
-    t->x = x;
-    t->mn = x;
-    t->y = rand();
+    t->sz = 1;
+    t->x = t->s = x;
+    t->y = rnd();
     return t;
 }
 
-int mn(ptr t){
-    return t == nullptr ? inf : t->mn;
+int sz(ptr t) {
+    return t == nullptr ? 0 : t->sz;
 }
 
-void upd(ptr &t){
-    if (t != nullptr)
-        t->mn = min(t->x, min(mn(t->l), mn(t->r)));
+ll sum(ptr t) {
+    return t == nullptr ? 0 : t->s;
 }
 
-void split(ptr t, ptr &l, ptr &r, int key){
-    if (t == nullptr){
+void upd(ptr &t) {
+    if (t != nullptr) {
+        t->sz = 1 + sz(t->l) + sz(t->r);
+        t->s = t->x + sum(t->l) + sum(t->r);
+    }
+}
+
+void split(ptr t, ptr &l, ptr &r, ll key) {
+    if (t == nullptr) {
         l = nullptr;
         r = nullptr;
         return;
     }
-    upd(t);
     if (t->x <= key)
         split(t->r, t->r, r, key), l = t;
     else
         split(t->l, l, t->l, key), r = t;
     upd(l);
     upd(r);
-    upd(t);
 }
 
-void merge(ptr &t, ptr l, ptr r){
-    if (l == nullptr){
+void ksplit(ptr t, ptr &l, ptr &r, int key) {
+    if (t == nullptr) {
+        l = nullptr;
+        r = nullptr;
+        return;
+    }
+    if (sz(t->l) < key)
+        ksplit(t->r, t->r, r, key - sz(t->l) - 1), l = t;
+    else
+        ksplit(t->l, l, t->l, key), r = t;
+    upd(l);
+    upd(r);
+}
+
+void merge(ptr &t, ptr l, ptr r) {
+    if (l == nullptr) {
         t = r;
         return;
     }
-    if (r == nullptr){
+    if (r == nullptr) {
         t = l;
         return;
     }
-    upd(l);
-    upd(r);
     if (l->y <= r->y)
         merge(l->r, l->r, r), t = l;
     else
         merge(r->l, l, r->l), t = r;
     upd(t);
+}
+
+void insert(ptr &root, ll w) {
+    ptr t = create(w);
+    ptr t1, t2;
+    split(root, t1, t2, w);
+    merge(root, t1, t);
+    merge(root, root, t2);
+}
+
+void go(ptr t) {
+    if (t == nullptr)
+        return;
+    go(t->l);
+    cout << t->x << ' ';
+    go(t->r);
 }
