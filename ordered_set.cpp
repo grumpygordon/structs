d4625304f6d262366289fa86ff77a342aa69850f
grumpy_gordon <ramil-pauchok@mail.ru>
2020-12-20 17:27:54 +0300

    ordered set

---
+++ ordered_set.cpp
@@ -0,0 +1,18 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+// find_by_order, order_of_key
+
+
+// usage:
+
+// ordered_set<T> q;
+
+// q.insert(z)
+// each element is inserted only if not present
+
+
+// q.order_of_key(z)
+// index(0-based) of lower_bound(z)
