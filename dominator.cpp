f74105df3d8eea6308c93c5d627d12fa68057f29
grumpy_gordon <ramil-pauchok@mail.ru>
2021-04-02 16:52:43 +0300

    fixaasssssss

--- dominator.cpp
+++ dominator.cpp
@@ -1,91 +1,94 @@
+//root required, not all nodes need to be reachable
 
 namespace Dominator {
-// source: https://tanujkhattar.wordpress.com/2016/01/11/dominator-tree-of-a-directed-graph
-// tree[v] - dominator tree
-// N > n
-typedef pair<int,int>   II;
-typedef vector< II >      VII;
-typedef vector<int>     VI;
-typedef vector< VI > 	VVI;
-typedef long long int 	LL;
 
-#define PB push_back
-#define MP make_pair
-#define F first
-#define S second
-#define SZ(a) (int)(a.size())
-#define ALL(a) a.begin(),a.end()
-#define SET(a,b) memset(a,b,sizeof(a))
-#define N maxn
-VI g[N];
-VI tree[N],rg[N],bucket[N];
-int sdom[N],par[N],dom[N],dsu[N],label[N];
-int arr[N],rev[N],T;
-int Find(int u,int x=0)
-{
-    if(u==dsu[u])return x?-1:u;
-    int v = Find(dsu[u],x+1);
-    if(v<0)return u;
-    if(sdom[label[dsu[u]]] < sdom[label[u]])
-        label[u] = label[dsu[u]];
-    dsu[u] = v;
-    return x?v:label[u];
-}
-void Union(int u,int v) //Add an edge u-->v
-{
-    dsu[v]=u; 	//yup,its correct :)
-}
-void dfs0(int u)
-{
-    //cerr << u << '\n';
-    T++;arr[u]=T;rev[T]=u;
-    label[T]=T;sdom[T]=T;dsu[T]=T;
-    for(int i=0;i<SZ(g[u]);i++)
-    {
-        int w = g[u][i];
-        if(!arr[w])dfs0(w),par[arr[w]]=arr[u];
-        rg[arr[w]].PB(arr[u]);
-    }
-}
+	const int N = maxn;
 
-void calculate(vector<vector<int>> edges, int root) {
-    int n = edges.size();
-    for (int i = 0; i < n; i++)
-        g[i] = edges[i];
-    //Build Dominator tree
-    dfs0(root);
-    n = T;
-    for (int i = n; i >= 1; i--) {
-        for (int j = 0; j < SZ(rg[i]); j++)
-            sdom[i] = min(sdom[i], sdom[Find(rg[i][j])]);
-        if (i > 1)bucket[sdom[i]].PB(i);
-        for (int j = 0; j < SZ(bucket[i]); j++) {
-            int w = bucket[i][j];
-            int v = Find(w);
-            if (sdom[v] == sdom[w])dom[w] = sdom[w];
-            else dom[w] = v;
-        }
-        if (i > 1)Union(par[i], i);
-    }
-    for (int i = 2; i <= n; i++) {
-        if (dom[i] != sdom[i])
-            dom[i] = dom[dom[i]];
-        tree[rev[i]].PB(rev[dom[i]]);
-        tree[rev[dom[i]]].PB(rev[i]);
-    }
-}
-    typedef pair<int,int>   II;
-    typedef vector< II >      VII;
-    typedef vector<int>     VI;
-    typedef vector< VI > 	VVI;
-    typedef long long int 	LL;
+	vector<int> bucket[N], gi[N], g[N];
+	vector<vector<int>> tree;
+	int sdom[N], dom[N], par[N], label[N], dsu[N], rev[N], arr[N];
 
-#undef PB
-#undef MP
-#undef F
-#undef S
-#undef SZ
-#undef ALL
-#undef SET
-#undef N
+	int tm;
+
+	int ask(int u, int x) {
+		if (u == dsu[u]) return x ? -1 : u;
+		int v = ask(dsu[u], x + 1);
+		if (v < 0) return u;
+		if (sdom[label[dsu[u]]] < sdom[label[u]])
+			label[u] = label[dsu[u]];
+		dsu[u] = v;
+		return x ? v : label[u];
+	};
+	void un(int u, int v) {
+		dsu[v] = u;
+	};
+
+	void dfs(int v) {
+		arr[v] = tm;
+		rev[tm] = v;
+		label[tm] = sdom[tm] = dsu[tm] = tm;
+		++tm;
+		for (int k : g[v]) {
+			if (arr[k] == -1) {
+				dfs(k);
+				par[arr[k]] = arr[v];
+			}
+			gi[arr[k]].push_back(arr[v]);
+		}
+	};
+
+
+	vector<vector<int>> dominator_tree(vector<vector<int>> const &g_, int root) {
+		tm = 0;
+		int n = g_.size();
+		tree.clear();
+		tree.resize(n);
+		for (int i = 0; i < n; i++)
+			g[i] = g_[i];
+		vector<int> p(n);
+		for (int i = 0; i < n; ++i) {
+			p[i] = i;
+			arr[i] = -1;
+		}
+		swap(p[root], p[0]);
+		swap(g[0], g[root]);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < g[i].size(); ++j) {
+				g[i][j] = p[g[i][j]];
+			}
+		}
+
+		dfs(0);
+		//tm - number of reachable nodes
+
+		for (int i = tm - 1; i >= 0; --i) {
+			for (int j : gi[i]) {
+				sdom[i] = min(sdom[i], sdom[ask(j, 0)]);
+			}
+			if (i != 0) bucket[sdom[i]].push_back(i);
+			for (int w : bucket[i]) {
+				int v = ask(w, 0);
+				if (sdom[v] == sdom[w]) dom[w] = sdom[w];
+				else dom[w] = v;
+			}
+			if (i != 0) un(par[i], i);
+		}
+		for (int i = 1; i < tm; ++i) {
+			if (dom[i] != sdom[i])
+				dom[i] = dom[dom[i]];
+			// ребро вниз
+			tree[rev[dom[i]]].push_back(rev[i]);
+			// если нужно неориентированное дерево
+			// tree[rev[i]].push_back(rev[dom[i]]);
+		}
+
+		swap(tree[root], tree[0]);
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < tree[i].size(); ++j) {
+				tree[i][j] = p[tree[i][j]];
+			}
+		}
+
+		return tree;
+	}
 }
