6dc445214afd559d7b066157f5525373c2f4b732
grumpy_gordon <ramil-pauchok@mail.ru>
2021-05-29 12:32:43 +0300

    simd guide

---
+++ simd.cpp
@@ -0,0 +1,3 @@
+// my example with mm256 as bitset: https://codeforces.com/contest/1531/submission/117753775
+// intel reference https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=AVX,AVX2
+// algorithmica guide https://algorithmica.org/ru/sse
