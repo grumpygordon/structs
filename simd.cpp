eb305640a4345c518a6e6a157c59721fa012fa50
grumpy_gordon <ramil-pauchok@mail.ru>
2021-05-29 13:19:03 +0300

    added example

--- simd.cpp
+++ simd.cpp
@@ -1,3 +1,4 @@
 // my example with mm256 as bitset: https://codeforces.com/contest/1531/submission/117753775
 // intel reference https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=AVX,AVX2
 // algorithmica guide https://algorithmica.org/ru/sse
+// kactl example https://github.com/koosaga/olympiad/blob/b14590b5e75d8a053d5c109423f2f757c4ee969c/Library/icpc-book-master/content/various/SIMD.h
